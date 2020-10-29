20958f0dac2879a45e5bd999d4ea1a9f36bd07f1
Marlon Mora <marlonmora.ndr@gmail.com>
2020-10-29 15:34:32 -0300

    installed sass through buildpack now css sass and bulma working together using style loader css loader and sass loader with babel loader

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navigation from "./components/Navbar/Navigation"
 import Hero from "./components/Hero/Hero"
 import Navbar from "react-bulma-components/lib/components/navbar";
-
+import "./sass/main.scss"
 
 const App = () => {
     return (
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader?sourceMap" // compiles Sass to CSS
+        }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map'
+}
