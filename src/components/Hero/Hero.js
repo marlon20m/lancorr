2b04d43b9c2d7c1489ca18799f4524d2d0a80c95
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-03 14:43:33 -0300

    Hero with green bg set to bulma is-primary created and lorem ipsum added. Looking great

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,21 +8,23 @@ function Hero() {
     <figure class="image is-5by3">
       <img src="https://i.ibb.co/cXGt9xW/jj-ying-7-JX0-bfiux-Q-unsplash-4.jpg" />
     </figure>
-    <figure class="image is-3by2">
-      <img src="https://i.ibb.co/ysF6xF1/shutterstock-546792490.jpg" />
-    </figure>
     <section class="hero is-medium is-primary is-bold">
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
-        Primary bold title
+        Esse aliqua ad eiusmod non Lorem do ut deserunt.
       </h1>
       <h2 class="subtitle">
-        Primary bold subtitle
+        Do tempor id consectetur magna proident elit sit labore consectetur do.
+        Irure laborum ea magna ipsum id quis cillum irure. Culpa reprehenderit aute occaecat reprehenderit aute non in incididunt nulla. Nostrud velit adipisicing culpa incididunt ipsum. Laborum qui aute sit elit laborum sint adipisicing sint ut veniam elit ullamco ipsum. In aute commodo sunt aute ea velit nisi nulla et nulla velit et consequat ad.
       </h2>
     </div>
   </div>
 </section>
+    <figure class="image is-3by2">
+      <img src="https://i.ibb.co/ysF6xF1/shutterstock-546792490.jpg" />
+    </figure>
+    
       </>
         )
       }
