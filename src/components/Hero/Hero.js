0da82f7c565dcd33a364e043e62840b206fe0dd4
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-03 11:16:13 -0300

    New responsive images added to Hero component. Now working on Hero text.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,16 +4,25 @@ import "react-bulma-components/lib/components/navbar";
 function Hero() {
   return (
       <>
-      <section className="hero is-fullheight">
-    <figure class="image is-1028x1028">
+      
+    <figure class="image is-5by3">
       <img src="https://i.ibb.co/cXGt9xW/jj-ying-7-JX0-bfiux-Q-unsplash-4.jpg" />
     </figure>
-      </section>
-      <section className="hero is-fullheight">
-    <figure class="image is-1028x1028">
+    <figure class="image is-3by2">
       <img src="https://i.ibb.co/ysF6xF1/shutterstock-546792490.jpg" />
     </figure>
-      </section>
+    <section class="hero is-medium is-primary is-bold">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Primary bold title
+      </h1>
+      <h2 class="subtitle">
+        Primary bold subtitle
+      </h2>
+    </div>
+  </div>
+</section>
       </>
         )
       }
