c476021f113038a97ea1706ce5ae0c1d84464a05
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-04 12:50:52 -0300

    Modified the hero sizes using is-large and is-medium in the hero class

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,26 +4,37 @@ import "react-bulma-components/lib/components/navbar";
 function Hero() {
   return (
       <>
-      
-    <figure class="image is-5by3">
-      <img src="https://i.ibb.co/cXGt9xW/jj-ying-7-JX0-bfiux-Q-unsplash-4.jpg" />
-    </figure>
-    <section class="hero is-medium is-primary is-bold">
+    <section class="hero is-large has-bg-img"> 
+      <div class="hero-body"> 
+        <div class="container has-text-centered"> 
+        <h1 class="title">Plastics Engineering</h1> 
+        </div> 
+      </div> 
+    </section>
+    <section class="hero is-medium is-bold">
   <div class="hero-body">
     <div class="container">
-      <h1 class="title">
+      <h1 class="title has-text-white">
         Esse aliqua ad eiusmod non Lorem do ut deserunt.
       </h1>
-      <h2 class="subtitle">
+      <h2 class="subtitle has-text-white">
         Do tempor id consectetur magna proident elit sit labore consectetur do.
         Irure laborum ea magna ipsum id quis cillum irure. Culpa reprehenderit aute occaecat reprehenderit aute non in incididunt nulla. Nostrud velit adipisicing culpa incididunt ipsum. Laborum qui aute sit elit laborum sint adipisicing sint ut veniam elit ullamco ipsum. In aute commodo sunt aute ea velit nisi nulla et nulla velit et consequat ad.
       </h2>
     </div>
   </div>
 </section>
-    <figure class="image is-3by2">
-      <img className="masterbatchImage" src="https://i.ibb.co/ysF6xF1/shutterstock-546792490.jpg" />
-    </figure>
+    <section class="hero is-medium has-bg-img2"> 
+      <div class="hero-body"> 
+        <div class="container has-text-left"> 
+          <div className="column is-one-third">
+        <h1 class="title">Title 1</h1> 
+        <h2 class="subtitle">Exercitation irure reprehenderit pariatur reprehenderit ullamco quis tempor officia exercitation aliqua Lorem. Commodo amet ex commodo do adipisicing mollit adipisicing ipsum. Cupidatat elit consectetur elit commodo consequat aute duis pariatur cillum tempor labore anim. Enim quis anim est dolore excepteur. Id consectetur laborum occaecat consectetur magna ut labore sunt.</h2> 
+          </div>
+        </div> 
+      </div> 
+    </section>
+  
     
       </>
         )
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -13,7 +13,7 @@ function Modal() {
             <div className="card">
         <div className="card-image">
           <figure className="image is-4by3">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+            <img src="https://i.ibb.co/W6N2FJn/mohan-murugesan-a-PUi7w4k-Kb-I-unsplash.jpg" alt="Placeholder image" />
           </figure>
         </div>
         <div className="card-content">
