abda9d2e8e76b15e8c8c69afcc94227acfcc323c
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-08 12:59:45 -0300

    Image for Inicio masterbatch was tuned down to medium size. First image was too large and would take too long to load

--- src/pages/Produtos/Masterbatches.js
+++ src/pages/Produtos/Masterbatches.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from "../../components/Navbar/Navigation"
+import Modal from "../../components/Modal/Modal"
 import "react-bulma-components/lib/components/navbar";
 import "../../sass/main.scss";
 
 function Masterbatches() {
     return (
         <>
-     <Navigation />
+    <Navigation />
+    <Modal />
         </>
     )
 }
