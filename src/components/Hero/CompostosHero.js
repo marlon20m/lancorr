47db02c3db7a2ccc723e106df9184fd62ef0aa1a
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-08 16:32:52 -0300

    Addition of Compostos Hero component for Compostos Termoplasticos

---
+++ src/components/Hero/CompostosHero.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "react-bulma-components/lib/components/navbar";
+
+function CompostosHero() {
+  return (
+      <>
+    <section class="hero is-large aditivos-hero-one is-large-mobile "> 
+      <div class="hero-body"> 
+        <div class="container has-text-left"> 
+        <h1 class="title has-text-white has-text-weight-bold title-text-ah is-size-1 is-size-3-mobile is-uppercase">
+        Compostos<br />
+        Termoplásticos
+      </h1>
+        </div> 
+      </div> 
+    </section>
+    <section class="hero is-small is-bold">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title has-text-white is">
+        Esse aliqua ad eiusmod non Lorem do ut deserunt.
+      </h1>
+      <h2 class="subtitle has-text-white">
+        Do tempor id consectetur magna proident elit sit labore consectetur do.
+        Irure laborum ea magna ipsum id quis cillum irure. Culpa reprehenderit aute occaecat reprehenderit aute non in incididunt nulla. Nostrud velit adipisicing culpa incididunt ipsum. Laborum qui aute sit elit laborum sint adipisicing sint ut veniam elit ullamco ipsum. In aute commodo sunt aute ea velit nisi nulla et nulla velit et consequat ad.
+      </h2>
+    </div>
+  </div>
+</section>
+      </>
+        )
+      }
+      
+
+export default CompostosHero;
