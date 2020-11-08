4ce7b94cf12306a83278d80c3501c1a5efe2eb50
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-08 16:29:31 -0300

    link for Aditivos biodegradaveis component created and updated hero to match Aditivos funcionais

--- src/pages/Produtos/AditivosBiodegradaveis.js
+++ src/pages/Produtos/AditivosBiodegradaveis.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from "../../components/Navbar/Navigation";
 import ProductCards from "../../components/ProductCards/ProductCards"
+import AditivosBioHero from "../../components/Hero/AditivosBioHero";
 import "react-bulma-components/lib/components/navbar";
 import "../../sass/main.scss";
 
@@ -8,6 +9,7 @@ function AditivosBiodegradaveis() {
     return (
         <>
      <Navigation />
+     <AditivosBioHero />
      <ProductCards />
         </>
     )
