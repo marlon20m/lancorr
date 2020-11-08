fec36c6d03ec9ad2ae0c8d19d2faa56189593da9
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-08 16:51:40 -0300

    Resinas Hero added to Resinas Petroquimicas. Now each card will have to be linked to Product selected

--- src/pages/Produtos/ResinasPetroquimicas.js
+++ src/pages/Produtos/ResinasPetroquimicas.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from "../../components/Navbar/Navigation";
-import ProductCards from "../../components/ProductCards/ProductCards"
+import ProductCards from "../../components/ProductCards/ProductCards";
+import ResinasHero from "../../components/Hero/ResinasHero";
 import "react-bulma-components/lib/components/navbar";
 import "../../sass/main.scss";
 
@@ -8,6 +9,7 @@ function ResinasPetroquimicas() {
     return (
         <>
      <Navigation />
+     <ResinasHero />
      <ProductCards />
         </>
     )
