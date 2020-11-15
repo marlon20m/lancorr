4958a32df9920954f07dc4d99d7a0ca91433caf9
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-15 14:03:17 -0300

    variable.sass contents were deleted due to error and unnessecary file

---
+++ client/build/precache-manifest.6ea93c761bf555c608b27abc3a368bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81297e33ab7937570f92ab6261514301",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25e04431fea69567c74b",
+    "url": "/static/css/2.7003f3d1.chunk.css"
+  },
+  {
+    "revision": "449f2dff71ae8a2ccffd",
+    "url": "/static/css/main.172af658.chunk.css"
+  },
+  {
+    "revision": "25e04431fea69567c74b",
+    "url": "/static/js/2.36d25bf5.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.36d25bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449f2dff71ae8a2ccffd",
+    "url": "/static/js/main.191cf6a1.chunk.js"
+  },
+  {
+    "revision": "b3e6e35fff37aa980de0",
+    "url": "/static/js/runtime-main.e5fcaf90.js"
+  }
+]);
