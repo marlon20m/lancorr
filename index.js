10e25690d3299586aaae95b4b3c45d454bc63f9a
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-16 13:03:15 -0300

    index.js issues resolved on dev

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const environment = process.env.NODE_ENV || "development";
-
+const PORT = process.env.PORT || 3001;
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 
@@ -72,12 +73,10 @@ if (environment === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-})
+app.use(express.static(path.join(__dirname, '../build')));
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
 })
