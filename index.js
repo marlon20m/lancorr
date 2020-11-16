f862113195a49a0e5616a9a17c864bec804c2a98
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-16 17:33:58 -0300

    Enabling CORS Pre-Flight

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
+app.options('*', cors())
 
 app.get('/', (req, res, next) => {
     res.json({message: "welcome to my contact form"});
