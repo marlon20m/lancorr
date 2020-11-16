4ccdbcf0be41aa06230cd1fb987869f00389e44e
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-16 16:26:27 -0300

    error with nodemailer and cors permissions so index.js changed with cors setting

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.options('*', cors());
 app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', (req, res, next) => {
     res.json({message: "welcome to my contact form"});
