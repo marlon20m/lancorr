17b0eae5f9600d688513eb6a1383dde9947776e1
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-17 17:09:20 -0300

    new issue heroku proxy turned to 3001

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors());
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
