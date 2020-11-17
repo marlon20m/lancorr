be23cd1a6b50213ad6beaf36675b9c25bb04659a
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-17 12:48:17 -0300

    Access-Control added for cors error on heroku

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.get('/', (req, res, next) => {
     res.json({message: "welcome to my contact form"});
 })
