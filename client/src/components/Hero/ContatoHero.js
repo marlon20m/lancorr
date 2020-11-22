7de0779a26852e360a57911a01bba4a5b7ec1fc1
Marlon Mora <marlonmora.ndr@gmail.com>
2020-11-22 20:11:21 -0300

    Successful deployed to heroku but nodemailer not working possibly because localhost:3000 is part of axios post still so now it will be /api/form

--- client/src/components/Hero/ContatoHero.js
+++ client/src/components/Hero/ContatoHero.js
@@ -37,7 +37,7 @@ export default class ContatoHero extends Component {
       email:this.state.email,
       message:this.state.message
     }
-      axios.post("http://localhost:3001/api/form", data)
+      axios.post("/api/form", data)
     .then(res =>{
       this.setState({ 
         sent:true,
