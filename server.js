340cbc877c3c009077435c592ede9097e2869345
Marlon Mora <marlonmora.ndr@gmail.com>
2021-01-30 13:30:13 -0300

    Change to outlook email

--- server.js
+++ server.js
@@ -11,36 +11,35 @@ const app = express();
 console.log(process.env);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 
-app.options("/send", cors()); 
+app.options("/send", cors());
 app.get("/send", cors(), (req, res, next) => {
-    res.json({message: "welcome to my contact form"});
+    res.json({ message: "welcome to my contact form" });
 })
 
 app.options("/api/form", cors())
+
 app.post("/api/form", function (req, res, next) {
 
 
     let data = req.body
     let smtpTransport = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      secure: true,
-      port: 465,
+        host: "smtp-mail.outlook.com",
+        port: 587,
         auth:{
-            user: process.env.USEREMAIL,
+            user: process.env.USEREMAIL, 
             pass: process.env.PASS
         }
     })
-
 
 let mailOptions = {
     from: data.email,
@@ -57,17 +56,12 @@ let mailOptions = {
         <li>Message: ${data.message}</li>
         <br></br>
         </ul>
-
     <h3>Mensagem</h3>
         <h4>${data.message}</h4>
-
     `
     }
 
-
-
-
-smtpTransport.sendMail(mailOptions, (error, response)=>{
+smtpTransport.sendMail(mailOptions, (error,res)=>{
 
         if(error){
             res.send(error)
@@ -77,14 +71,13 @@ smtpTransport.sendMail(mailOptions, (error, response)=>{
         }
 })
 
-smtpTransport.close();
 
 })
 
 
-console.log(process.env.NODE_ENV);
+
 if (environment === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 app.use(express.static(path.join(__dirname, '../build')));
@@ -92,6 +85,6 @@ app.use(express.static(path.join(__dirname, '../build')));
 app.set("port", PORT);
 
 app.listen(PORT, err => {
-    
+
     console.log(`App running on port ${PORT}!`);
-  });
+});
