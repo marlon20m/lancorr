1c5a149a101da7c3332f77df8e34da6998c36a31
Marlon Mora <marlonmora.ndr@gmail.com>
2021-01-31 14:56:40 -0300

    Removed express.use as client build name

--- server.js
+++ server.js
@@ -83,8 +83,6 @@ if (environment === "production") {
     app.use(express.static("client/build"));
 }
 
-app.use(express.static(path.join(__dirname, "./client/build/index.html")));
-
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
