a1751e75219e76a9fb07676ad5be0390a89d94f8
Marlon Mora <marlonmora.ndr@gmail.com>
2021-02-01 18:15:50 -0300

    fixed up the mobile issue in the products component

--- client/src/components/Hero/AditivosBioHero.js
+++ client/src/components/Hero/AditivosBioHero.js
@@ -4,31 +4,35 @@ import "react-bulma-components/lib/components/navbar";
 function AditivosBioHero() {
   return (
     <>
-    <section className="hero is-large bio-hero"> 
-      <div className="hero-body"> 
-        <div className="container has-text-left"> 
-        <h1 className="title has-text-white has-text-weight-bold title-text-ah is-size-1 is-size-3-mobile is-uppercase">
-        Aditivos <br />
-        Biodegradaveis
-      </h1>
-        </div> 
-      </div> 
-    </section>
-    <section className="hero is-small is-bold">
-  <div className="hero-body">
-    <div className="container">
-      <h1 className="title has-text-white is">
-      Aditivos Biodegradaveis      </h1>
-      <h2 className="subtitle has-text-white">
-      Através da aplicação de concentrados de cor, formulados nos mais diversos tipos de polímeros, obtém-se características diferenciadas
-    como: pigmentação/cores, efeitos, e acabamentos especiais.
+      <section className="hero bio-hero is-large has-bg-img">
+        <div className="hero-body">
+          <div className="container has-text-left">
+            <br />
+            <h1 className="title has-text-white has-text-weight-bold title-text-ah is-size-1 is-size-3-mobile is-uppercase">
+              Aditivos <br /> Biodegradaveis                </h1>
+            <br />
+            <br />
+            <br />
+          </div>
+        </div>
+      </section>
+      <section className="hero is-small is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title has-text-white is">
+              Aditivos Biodegradaveis      </h1>
+            <h2 className="subtitle has-text-white">
+              Através da aplicação de concentrados de cor, formulados nos mais diversos tipos de polímeros, obtém-se características diferenciadas
+              como: pigmentação/cores, efeitos, e acabamentos especiais.
       </h2>
-    </div>
-  </div>
-</section>
-      </>
-        )
-      }
-      
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
 
 export default AditivosBioHero;
+
+
--- client/src/components/Hero/CompostosHero.js
+++ client/src/components/Hero/CompostosHero.js
@@ -3,32 +3,35 @@ import "react-bulma-components/lib/components/navbar";
 
 function CompostosHero() {
   return (
-      <>
-    <section className="hero is-large compostos-hero is-large-mobile "> 
-      <div className="hero-body"> 
-        <div className="container has-text-left"> 
-        <h1 className="title has-text-white has-text-weight-bold title-text-ah is-size-1 is-size-3-mobile is-uppercase">
-        Compostos<br />
+    <>
+      <section className="hero is-large compostos-hero is-large-mobile ">
+        <div className="hero-body">
+          <div className="container has-text-left">
+            <h1 className="title has-text-white has-text-weight-bold title-text-ah is-size-1 is-size-3-mobile is-uppercase">
+              Compostos<br />
         Termoplásticos
       </h1>
-        </div> 
-      </div> 
-    </section>
-    <section className="hero is-small is-bold">
-  <div className="hero-body">
-    <div className="container">
-      <h1 className="title has-text-white is">
-      Compostos Termoplásticos      </h1>
-      <h2 className="subtitle has-text-white">
-      Os compostos termoplásticos são formulados a partir de blendas de polímeros, aditivos, reforços, e outros insumos, que incorporados à resina base
-    modificam a sua estrutura original proporcionando melhor desempenho técnico.
+            <br />
+            <br />
+            <br />
+          </div>
+        </div>
+      </section>
+      <section className="hero is-small is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title has-text-white is">
+              Compostos Termoplásticos      </h1>
+            <h2 className="subtitle has-text-white">
+              Os compostos termoplásticos são formulados a partir de blendas de polímeros, aditivos, reforços, e outros insumos, que incorporados à resina base
+              modificam a sua estrutura original proporcionando melhor desempenho técnico.
       </h2>
-    </div>
-  </div>
-</section>
-      </>
-        )
-      }
-      
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
 
 export default CompostosHero;
