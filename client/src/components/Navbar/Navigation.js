6427687b9b1d59bd8bf4ae178d0c4817efb64770
Marlon Mora <marlonmora.ndr@gmail.com>
2021-02-02 16:46:12 -0300

    new webpage added to nav-brand target logo link

--- client/src/components/Navbar/Navigation.js
+++ client/src/components/Navbar/Navigation.js
@@ -10,7 +10,7 @@ const Navigation = (props) => {
   return (
     <nav className="navbar fixed-navbar is-spaced navbar-bg" role="navigation" aria-label="main navigation">
   <div className="navbar-brand">
-    <a className="navbar-item-img-max-height" href="https://bulma.io">
+    <a className="navbar-item-img-max-height" href="https://www.lancorr.com">
       <img
         className="lancorrLogo" src="https://i.ibb.co/DkLN9KG/Lan-Corr-Logo.jpg" alt="Logo"
         />
