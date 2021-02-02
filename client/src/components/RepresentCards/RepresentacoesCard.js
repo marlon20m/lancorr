8792cdd0c4f9570ef4f7407a80fd8cdd36978989
Marlon Mora <marlonmora.ndr@gmail.com>
2021-02-02 18:32:00 -0300

    update to beplast and tritec description

--- client/src/components/RepresentCards/RepresentacoesCard.js
+++ client/src/components/RepresentCards/RepresentacoesCard.js
@@ -3,129 +3,133 @@ import "react-bulma-components/lib/components/navbar";
 import EcoVentures from "../../pages/Representacoes/EcoVentures";
 
 function RepresentacoesCard() {
-        
-        return(
-            <>
-            <br />
-            <br />
-        <section className="blog-posts" id={"section1"}>
-          <div className="container">
-            <div className="columns">
-              <div className="column is-10 is-offset-1">
-                <div className="columns featured-post is-multiline">
-                  <div className="column is-12 post">
-                    <article className="columns featured">
-                     
-                      <div className="column is-5 featured-content va">
-                        <div>
-                          <h3 className="heading post-category">Company Name</h3>
-                          <h1 className="title post-title has-text-link">Beplast</h1>
-                          <p className="post-excerpt">This template is based off of the official default blog template created by the fine folks over at <a href="https://ghost.io">Ghost</a>. If you are looking for a fully featured blog platform, I highly recommend checking them out!</p>
-                          <br />
-                          <a href="http://beplast.com.br" target="_blank" className="button is-primary">Website</a>
-                        </div>
+
+  return (
+    <>
+      <br />
+      <br />
+      <section className="blog-posts" id={"section1"}>
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="columns featured-post is-multiline">
+                <div className="column is-12 post">
+                  <article className="columns featured">
+
+                    <div className="column is-5 featured-content va">
+                      <div>
+                        <h3 className="heading post-category">Company Name</h3>
+                        <h1 className="title post-title has-text-link">Beplast</h1>
+                        <p className="post-excerpt">A empresa desenvolve soluções customizadas em aditivos, compostos termoplásticos, e masterbatches, para a indústria de transformação
+                        de plásticos, sendo um referencial de excelência no mercado nacional. Apresenta duas unidades industriais estabelecidas em São Leopoldo/RS,
+    e Sobral/CE, que atendem a diversos segmentos: agrícola; automotivo; calçadista; utilidades domésticas; eletroeletrônico; embalagens; etc.</p>
+                        <br />
+                        <a href="http://beplast.com.br" target="_blank" className="button is-primary">Website</a>
                       </div>
-                      <div className="column is-7 post-img ">
-                        <img src="https://i.ibb.co/myr83xp/Screen-Shot-2020-11-08-at-8-05-24-PM.png" alt="Beplast" />
-                      </div>
-                    </article>
-                  </div>
+                    </div>
+                    <div className="column is-7 post-img ">
+                      <img src="https://i.ibb.co/myr83xp/Screen-Shot-2020-11-08-at-8-05-24-PM.png" alt="Beplast" />
+                    </div>
+                  </article>
                 </div>
-                <hr />
               </div>
+              <hr />
             </div>
           </div>
-          </section>
-          <section className="blog-posts" id={"section2"}>
-          <div className="container">
-            <div className="columns">
-              <div className="column is-10 is-offset-1">
-                <div className="columns featured-post is-multiline">
-                  <div className="column is-12 post">
-                    <article className="columns featured">
-                     
-                      <div className="column is-5 featured-content va">
-                        <div>
-                          <h3 className="heading post-category">Company Name</h3>
-                          <h1 className="title post-title has-text-link">Eco Ventures Bioplastics</h1>
-                          <p className="post-excerpt">This template is based off of the official default blog template created by the fine folks over at <a href="https://ghost.io">Ghost</a>. If you are looking for a fully featured blog platform, I highly recommend checking them out!</p>
-                          <br />
-                          <a href="http://www.ecoventuresbrasil.com" target="_blank" className="button is-primary">Website</a>
-                        </div>
+        </div>
+      </section>
+      <section className="blog-posts" id={"section2"}>
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="columns featured-post is-multiline">
+                <div className="column is-12 post">
+                  <article className="columns featured">
+
+                    <div className="column is-5 featured-content va">
+                      <div>
+                        <h3 className="heading post-category">Company Name</h3>
+                        <h1 className="title post-title has-text-link">Eco Ventures Bioplastics</h1>
+                        <p className="post-excerpt">This template is based off of the official default blog template created by the fine folks over at <a href="https://ghost.io">Ghost</a>. If you are looking for a fully featured blog platform, I highly recommend checking them out!</p>
+                        <br />
+                        <a href="http://www.ecoventuresbrasil.com" target="_blank" className="button is-primary">Website</a>
                       </div>
-                      <div className="column is-7 post-img ">
-                        <img src="https://i.ibb.co/fpbdWM4/Screen-Shot-2020-11-08-at-8-57-25-PM.png" alt="Eco Ventures Bioplastics" />
-                      </div>
-                    </article>
-                  </div>
+                    </div>
+                    <div className="column is-7 post-img ">
+                      <img src="https://i.ibb.co/fpbdWM4/Screen-Shot-2020-11-08-at-8-57-25-PM.png" alt="Eco Ventures Bioplastics" />
+                    </div>
+                  </article>
                 </div>
-                <hr />
               </div>
+              <hr />
             </div>
           </div>
-          </section>
-          <section className="blog-posts" id={"section3"}>
-          <div className="container">
-            <div className="columns">
-              <div className="column is-10 is-offset-1">
-                <div className="columns featured-post is-multiline">
-                  <div className="column is-12 post">
-                    <article className="columns featured">
-                     
-                      <div className="column is-5 featured-content va">
-                        <div>
-                          <h3 className="heading post-category">Company Name</h3>
-                          <h1 className="title post-title has-text-link">Prisma</h1>
-                          <p className="post-excerpt">This template is based off of the official default blog template created by the fine folks over at <a href="https://ghost.io">Ghost</a>. If you are looking for a fully featured blog platform, I highly recommend checking them out!</p>
-                          <br />
-                          <a href="http://www.prismatermoplasticos.com.br" target="_blank" className="button is-primary">Website</a>
-                        </div>
+        </div>
+      </section>
+      <section className="blog-posts" id={"section3"}>
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="columns featured-post is-multiline">
+                <div className="column is-12 post">
+                  <article className="columns featured">
+
+                    <div className="column is-5 featured-content va">
+                      <div>
+                        <h3 className="heading post-category">Company Name</h3>
+                        <h1 className="title post-title has-text-link">Prisma</h1>
+                        <p className="post-excerpt">This template is based off of the official default blog template created by the fine folks over at <a href="https://ghost.io">Ghost</a>. If you are looking for a fully featured blog platform, I highly recommend checking them out!</p>
+                        <br />
+                        <a href="http://www.prismatermoplasticos.com.br" target="_blank" className="button is-primary">Website</a>
                       </div>
-                      <div className="column is-7 post-img ">
-                        <a href="http://www.prismatermoplasticos.com.br" target="_blank">
+                    </div>
+                    <div className="column is-7 post-img ">
+                      <a href="http://www.prismatermoplasticos.com.br" target="_blank">
                         <img src="https://i.ibb.co/D8f4LjH/Screen-Shot-2020-11-08-at-9-26-39-PM.png" alt="Eco Ventures Bioplastics" />
-                        </a>
-                      </div>
-                    </article>
-                  </div>
+                      </a>
+                    </div>
+                  </article>
                 </div>
-                <hr />
               </div>
+              <hr />
             </div>
           </div>
-          </section>
-          <section className="blog-posts" id={"section4"}>
-          <div className="container">
-            <div className="columns">
-              <div className="column is-10 is-offset-1">
-                <div className="columns featured-post is-multiline">
-                  <div className="column is-12 post">
-                    <article className="columns featured">
-                     
-                      <div className="column is-5 featured-content va">
-                        <div>
-                          <h3 className="heading post-category">Company Name</h3>
-                          <h1 className="title post-title has-text-link">Tritec Resinas</h1>
-                          <p className="post-excerpt">This template is based off of the official default blog template created by the fine folks over at <a href="https://ghost.io">Ghost</a>. If you are looking for a fully featured blog platform, I highly recommend checking them out!</p>
-                          <br />
-                         
-                        </div>
+        </div>
+      </section>
+      <section className="blog-posts" id={"section4"}>
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="columns featured-post is-multiline">
+                <div className="column is-12 post">
+                  <article className="columns featured">
+
+                    <div className="column is-5 featured-content va">
+                      <div>
+                        <h3 className="heading post-category">Company Name</h3>
+                        <h1 className="title post-title has-text-link">Tritec Resinas</h1>
+                        <p className="post-excerpt">Através de acordos comerciais com os principais fornecedores internacionais, a Tritec distribui e comercializa resinas termoplásticas das mais
+                        renomadas marcas: Asia Chemical; Lyondellbasell; LG; Exxon; Formosa Plastics; Esenttia; REPSOL; SCG; Montachem; SABIC; entre outros.
+    Atuando há 30(trinta) anos no mercado de transformação de plásticos, com destacada participação nas regiões sul e sudeste do Brasil.</p>
+                        <br />
+
                       </div>
-                      
-                    </article>
-                  </div>
+                    </div>
+
+                  </article>
                 </div>
-                <hr />
               </div>
+              <hr />
             </div>
           </div>
-          </section>
-    
-        
-                
-            </>
-         
-        )
+        </div>
+      </section>
+
+
+
+    </>
+
+  )
 }
 
 export default RepresentacoesCard;
