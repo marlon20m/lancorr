990114d63bcac2de68f83cf3abe418694547c9ef
Marlon Mora <marlonmora.ndr@gmail.com>
2021-02-05 16:08:41 -0300

    removed source code text in footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,9 +7,7 @@ function Hero() {
       <footer className="footer">
   <div className="content has-text-centered">
     <p>
-      <strong>Lancorr</strong> Web Design & Development by <a href="https://jgthms.com">Marlon A Mora</a>. The source code is licensed
-      <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
-      is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+      <strong>Lancorr</strong> Web Design & Development by <a href="https://marlonmoraportfolio.herokuapp.com/">Marlon A Mora</a>
     </p>
   </div>
  </footer>
