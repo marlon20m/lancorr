9f77daeb6df8dc4d7171c77027660a7dbc2db1f4
Marlon Mora <marlonmora.ndr@gmail.com>
2021-02-05 16:04:44 -0300

    updating Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ function Hero() {
       <footer className="footer">
   <div className="content has-text-centered">
     <p>
-      <strong>Lancorr</strong> by <a href="https://jgthms.com">Marlon A Mora</a>. The source code is licensed
+      <strong>Lancorr</strong> Web Design & Development by <a href="https://jgthms.com">Marlon A Mora</a>. The source code is licensed
       <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
       is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
     </p>
