08c9ebfcb0777c347324e82c75d03a79f029fbf2
Marlon Mora <marlonmora.ndr@gmail.com>
2021-02-19 12:34:44 -0500

    new build created

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+
 module.exports = {
     entry: './client/index.js',
     output: {
@@ -27,7 +29,24 @@ module.exports = {
     resolve: {
       extensions: ['.js', '.jsx']
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+      new webpack.DefinePlugin({ 
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ]
   }
 
   
